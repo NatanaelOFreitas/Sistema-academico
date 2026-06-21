4cbc708dcd2786680f269dc751ac1980614b2e9a
IanCohen1710 <iancohen9876@gmail.com>
2026-06-20 21:51:11 -0400

    Repository

---
+++ src/Repository/AlunoRepository.java
@@ -0,0 +1,101 @@
+package Repository;
+
+import Model.Aluno;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AlunoRepository {
+    private ArrayList<Aluno> alunos;
+
+    public AlunoRepository() {
+        this.alunos = new ArrayList<>();
+    }
+
+    public boolean adicionar(Aluno aluno) {
+        if(buscarPorMatricula(aluno.getMatricula())!=null) {
+            return false;
+        }
+        alunos.add(aluno);
+        return true;
+    }
+
+    public ArrayList<Aluno> listar() {
+        return alunos;
+    }
+
+    public ArrayList<Aluno> buscarPorNome(String nome) {
+        ArrayList<Aluno> encontrados = new ArrayList<>();
+        for(int i=0;i<alunos.size();i++) {
+            Aluno aluno = alunos.get(i);
+            if(aluno.getNome().toLowerCase().contains(nome.toLowerCase())) {
+                encontrados.add(aluno);
+            }
+        }
+        return encontrados;
+    }
+
+    public Aluno buscarPorMatricula(String matricula) {
+        for(int i=0;i<alunos.size();i++) {
+            Aluno aluno = alunos.get(i);
+            if(aluno.getMatricula().equals(matricula)) {
+                return aluno;
+            }
+        }
+        return null;
+    }
+
+    public boolean remover(String matricula) {
+        Aluno aluno = buscarPorMatricula(matricula);
+        if(aluno!=null) {
+            alunos.remove(aluno);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean atualizar(Aluno alunoAtualizado) {
+        Aluno aluno = buscarPorMatricula(alunoAtualizado.getMatricula());
+        if(aluno!=null) {
+            aluno.setNome(alunoAtualizado.getNome());
+            aluno.setCurso(alunoAtualizado.getCurso());
+            aluno.setPeriodo(alunoAtualizado.getPeriodo());
+            return true;
+        }
+        return false;
+    }
+
+    public void salvarArquivo(String caminho) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(caminho));
+            for(int i=0;i<alunos.size();i++) {
+                bw.write(alunos.get(i).toString());
+                bw.newLine();
+            }
+            bw.close();
+        }
+        catch(IOException e) {
+            System.out.println("Erro ao salvar arquivo");
+        }
+    }
+
+    public void carregarArquivo(String caminho) {
+        alunos.clear();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(caminho));
+            String linha;
+            while((linha = br.readLine()) != null) {
+                String[] dados = linha.split(";");
+                Aluno aluno = new Aluno(dados[1],dados[0],dados[2],Integer.parseInt(dados[3]));
+                alunos.add(aluno);
+            }
+            br.close();
+        }
+        catch(IOException e) {
+            System.out.println("Erro ao carregar arquivo");
+        }
+    }
+}
---
+++ src/Repository/DisciplinaRepository.java
@@ -0,0 +1,100 @@
+package Repository;
+
+import Model.Disciplina;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DisciplinaRepository {
+    private ArrayList<Disciplina> disciplinas;
+
+    public DisciplinaRepository() {
+        this.disciplinas = new ArrayList<>();
+    }
+
+    public boolean adicionar(Disciplina disciplina) {
+        if(buscarPorCodigo(disciplina.getCodigo()) != null) {
+            return false;
+        }
+        disciplinas.add(disciplina);
+        return true;
+    }
+
+    public ArrayList<Disciplina> listar() {
+        return disciplinas;
+    }
+
+    public Disciplina buscarPorCodigo(String codigo) {
+        for(int i=0;i<disciplinas.size();i++) {
+            Disciplina disciplina = disciplinas.get(i);
+            if(disciplina.getCodigo().equals(codigo)) {
+                return disciplina;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Disciplina> buscarPorNome(String nome) {
+        ArrayList<Disciplina> encontradas = new ArrayList<>();
+        for(int i=0;i<disciplinas.size();i++) {
+            Disciplina disciplina = disciplinas.get(i);
+            if(disciplina.getNome().toLowerCase().contains(nome.toLowerCase())) {
+                encontradas.add(disciplina);
+            }
+        }
+        return encontradas;
+    }
+
+    public boolean remover(String codigo) {
+        Disciplina disciplina = buscarPorCodigo(codigo);
+        if(disciplina!=null) {
+            disciplinas.remove(disciplina);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean atualizar(Disciplina disciplinaAtualizada) {
+        Disciplina disciplina = buscarPorCodigo(disciplinaAtualizada.getCodigo());
+        if(disciplina!=null) {
+            disciplina.setNome(disciplinaAtualizada.getNome());
+            disciplina.setCargaHoraria(disciplinaAtualizada.getCargaHoraria());
+            return true;
+        }
+        return false;
+    }
+
+    public void salvarArquivo(String caminho) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(caminho));
+            for(int i=0;i<disciplinas.size();i++) {
+                bw.write(disciplinas.get(i).toString());
+                bw.newLine();
+            }
+            bw.close();
+        }
+        catch(IOException e) {
+            System.out.println("Erro ao salvar arquivo");
+        }
+    }
+
+    public void carregarArquivo(String caminho) {
+        disciplinas.clear();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(caminho));
+            String linha;
+            while((linha = br.readLine()) != null) {
+                String[] dados = linha.split(";");
+                Disciplina disciplina = new Disciplina(dados[0],dados[1],Integer.parseInt(dados[2]));
+                disciplinas.add(disciplina);
+            }
+            br.close();
+        }
+        catch(IOException e) {
+            System.out.println("Erro ao carregar arquivo");
+        }
+    }
+}
